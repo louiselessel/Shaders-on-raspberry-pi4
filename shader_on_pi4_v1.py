dff3f82129492fb7e4e53af1249dfa4fa4b0af6c
Louise Lessel <louiselessel@gmail.com>
2020-07-16 16:36:23 -0400

    Added new test

--- shader_on_pi4_v1.py
+++ shader_on_pi4_v1.py
@@ -13,8 +13,9 @@ ZPLANE = 1000
 S_RES_W = 200
 S_RES_H = 200
 
-## EITHER Set size of window
+#-----------------------
 
+## EITHER Set size of window
 WIDTH = 200
 HEIGHT = 200
 
@@ -32,7 +33,8 @@ WIDTH, HEIGHT = DISPLAY.width, DISPLAY.height
 
 #shader = pi3d.Shader("shaders/test") # the most rudamentary shader - red color
 #shader = pi3d.Shader("shaders/filter_toon") # one of the pi3D shaders, using tex input
-shader = pi3d.Shader("shaders/test_shadertoy")
+shader = pi3d.Shader("shaders/test2_shadertoy")
+#shader = pi3d.Shader("shaders/TD_shadertoy")
 
 
 
--- shaders/test2_shadertoy.fs
+++ shaders/test2_shadertoy.fs
@@ -3,6 +3,7 @@
 precision mediump float;
 #endif
 
+// Add this in so I can pass in uniforms
 uniform vec3 unif[20]; 
 // is a vec3 so there is three spaces to place a uniform
 // for some reason they are in array place index 16
@@ -10,27 +11,9 @@ uniform vec3 unif[20];
 // 16,1 is uniform1
 // 16,2 is uniform2
 
-//----------------------
-// None of these methods work
-//uniform float     iTime;                 // shader playback time (in seconds)
-//uniform float iTime = unif[16][0];
-
-//#define t iTime;
-//float t = iTime;
-
-//#define t unif[16][0];
-
-//uniform float t;
-
-uniform float iTime => unif[16][0];
-
-//------------------------
-
-//layout (location = 0) out vec4 fragColor;
-
-// ShaderToy variables
+// ShaderToy variables boilerplate
 	uniform vec3      iResolution;           // viewport resolution (in pixels)
-	//uniform float     iTime;                 // shader playback time (in seconds)
+	uniform float     iTime;                 // shader playback time (in seconds)
 	uniform float     iTimeDelta;            // render time (in seconds)
 	uniform int       iFrame;                // shader playback frame
 	uniform float     iChannelTime[4];       // channel playback time (in seconds)
@@ -42,21 +25,32 @@ uniform float iTime => unif[16][0];
 	uniform sampler2D iChannel3;
 	uniform vec4      iDate;                 // (year, month, day, time in seconds)
  
+// Shader from shadertoy
 // http://www.pouet.net/prod.php?which=57245
 // If you intend to reuse this shader, please add credits to 'Danilo Guanabara'
+// http://www.shadertoy.com/view//XsXXDnv
 
+
+// Why don't #define work? If I comment them in, the code breaks
 //#define t iTime
 //#define r iResolution.xy
 
-
-
 void main() {
+	// The #defines don't work so instead I have been doing this.
+	// I found that I have to assign the unif[...] in the main loop?
 	float t = unif[16][0];
-	vec2 r;
-	r.x = unif[16][1];
-	r.y = unif[16][2];
-
+	vec2 r = vec2(unif[16][1],unif[16][2]);
 	
+	
+	// Additionally, actually using the uniforms from the
+	// shadertoy boilerplate doesnt work, and I can't assign them here?
+	// Commenting this in will break the code.
+	
+	//iResolution = vec3(unif[16][1],unif[16][2], 0.0); // dont work
+	//iResolution.xy = vec2(unif[16][1],unif[16][2]);	// dont work
+	
+	
+	// From the original shader code
 	vec3 c;
 	float l,z =t;
 	
@@ -71,9 +65,5 @@ void main() {
 		uv+=p/l*(sin(z)+1.)*abs(sin(l*9.-z*2.));
 		c[i]=.01/length(abs(mod(uv,1.)-.5));
 	}
-		
-	//gl_FragColor = vec4(t,0.0,0.0,1.0);
-	//gl_FragColor = vec4(l, r.y/r.y, r.x/r.x, 1.0);
-	//gl_FragColor = vec4(z, 0.0, 0.0, 1.0);
 	gl_FragColor=vec4(c/l,t);
 }
