973fa652ddda87a708eb3aba734f4eec6eb39fda
Louise Lessél <louiselessel@gmail.com>
2020-07-28 15:55:13 -0400

    Code from Paddy to scale
    To get downscaled version of shader

---
+++ New Test2/Paddy2.py
@@ -0,0 +1,35 @@
+import time
+import demo
+import pi3d
+
+(W, H) = (None, None) # None should fill the screen (there are edge issues)
+SCALE = 0.25 #should have 16th the shadertoy workload
+
+display = pi3d.Display.create(w=W, h=H, frames_per_second=60.0)
+print(display.opengl.gl_id)
+if W is None or H is None:
+ (W, H) = (display.width, display.height)
+sprite = pi3d.Triangle(corners=((-1.0, -1.0),(-1.0, 3.0),(3.0, -1.0)))
+shader = pi3d.Shader('shaders/shadertoy01')
+sprite.set_shader(shader)
+
+## offscreen texture stuff ##
+cam = pi3d.Camera(is_3d=False)
+flatsh = pi3d.Shader('shaders/post_vanilla')
+post = pi3d.PostProcess(camera=cam, shader=flatsh, scale=SCALE)
+
+kbd = pi3d.Keyboard()
+sprite.unif[0:2] = [W, H]
+sprite.unif[4] = SCALE
+tm0 = time.time()
+while display.loop_running():
+    post.start_capture()
+    sprite.draw()
+    post.end_capture()
+    post.draw()
+    sprite.unif[3] = time.time() - tm0
+    k = kbd.read()
+    if k == 27:
+        kbd.close()
+        display.stop()
+        break
