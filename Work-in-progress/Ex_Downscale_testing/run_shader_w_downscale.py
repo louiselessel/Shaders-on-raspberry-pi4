38ae42efbdbbf82b5e57d8775d72f4ddb744b761
Louise Lessel <louiselessel@gmail.com>
2020-08-12 18:45:24 -0400

    tested several shaders. They work but sometimes the offset is off

--- Work-in-progress/Ex_Downscale_testing/run_shader_w_downscale.py
+++ Work-in-progress/Ex_Downscale_testing/run_shader_w_downscale.py
@@ -2,42 +2,61 @@ import time
 import demo
 import pi3d
 
-## This code was made with help from paddywwoof
-
-#(W, H) = (None, None) # None should fill the screen (there are edge issues)
+#(W, H) = (None, None) # Fullscreen - None should fill the screen (there are edge issues)
 (W, H) = (400, 400) # Windowed
-
 # For scale, make sure the numbers are divisible to the resolution with no remainders (use even numbers between 0 and 1). 1.0 is full non-scaled resolution.
-SCALE = 0.2
+SCALE = .6 # downscale the shadertoy shader
 
 BACKGROUND_COLOR = (0.0, 0.0, 0.0, 0.0)
 
-display = pi3d.Display.create(w=W, h=H, frames_per_second=30.0, background=BACKGROUND_COLOR)
+display = pi3d.Display.create(w=W, h=H, frames_per_second=24.0,
+                              background=BACKGROUND_COLOR,
+                              display_config=pi3d.DISPLAY_CONFIG_HIDE_CURSOR | pi3d.DISPLAY_CONFIG_MAXIMIZED,
+                              use_glx=True)
 print(display.opengl.gl_id)
 if W is None or H is None:
  (W, H) = (display.width, display.height)
+ print('setting display size to ' + str(W) + ' ' + str(H))
+
+## shadertoy shader stuff ##
 sprite = pi3d.Triangle(corners=((-1.0, -1.0),(-1.0, 3.0),(3.0, -1.0)))
-shader = pi3d.Shader('shadertoy01')
+shader = pi3d.Shader('shadertoy01') # cloud shader
 sprite.set_shader(shader)
 
 ## offscreen texture stuff ##
 cam = pi3d.Camera(is_3d=False)
-flatsh = pi3d.Shader('post_vanilla')
-post = pi3d.PostProcess(camera=cam, shader=flatsh, scale=SCALE)
+postsh = pi3d.Shader('post_vanilla')
+post = pi3d.PostProcess(camera=cam, shader=postsh, scale=SCALE)
 
+## interactive input ##
+mouse = (0.9, 0.5) # hardcoded, replace these with interactive mouse xy coordinates
 kbd = pi3d.Keyboard()
-sprite.unif[0:2] = [W, H]
-sprite.unif[4] = SCALE
+
+# pass uniforms into our base shader
+sprite.unif[0:2] = [W, H] # iResolution
+sprite.unif[4] = SCALE # iScale
+sprite.unif[6:8] = [mouse[0], mouse[1]] # iMouse
+
+# pass uniforms into postprocessing postsh
+post.draw({0:W, 1:H, 4:SCALE, 6:mouse[0], 7:mouse[1]})
+
+# time at start
 tm0 = time.time()
 
 while display.loop_running():
+    # drawing
     post.start_capture()
     sprite.draw()
     post.end_capture()
     post.draw()
-    sprite.unif[3] = time.time() - tm0
+    # setting interactive / changing uniforms
+    sprite.unif[3] = (time.time() - tm0) * 1.0    # change the multiplier to slow time
+    sprite.unif[6:8] = [mouse[0], mouse[1]] 
+    post.draw({6:mouse[0], 7:mouse[1]})
+    # keyboard control
     k = kbd.read()
     if k == 27:
         kbd.close()
         display.stop()
         break
+
--- Work-in-progress/Ex_Downscale_testing/shadertoy01.fs
+++ Work-in-progress/Ex_Downscale_testing/shadertoy01.fs
@@ -7,93 +7,218 @@
 
 
 
-    #include std_head_fs.inc
-    #define iTime unif[1][0]
-    #define iResolution unif[0]
-    #define iScale unif[1][1]
-    #define iMouse unif[3]
+	#include std_head_fs.inc
+	#define iTime unif[1][0]
+	#define iResolution unif[0]
+	#define iScale unif[1][1]
+	#define iMouse unif[3]
+	
+ 
+
+
+ 
+
+ 
+// "Rainbow Snake" by Martijn Steinrucken aka BigWings - 2015
+// Email:countfrolic@gmail.com Twitter:@The_ArtOfCode
+// License Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+
+#define SIZE 10.
+#define PI 3.1415
+
+#define MOD3 vec3(.1031,.11369,.13787)
+float hash12(vec2 p) {
+     // From https://www.shadertoy.com/view/4djSRW
+	// Dave Hoskins
+	vec3 p3  = fract(vec3(p.xyx) * MOD3);
+    p3 += dot(p3, p3.yzx + 19.19);
+    return fract((p3.x + p3.y) * p3.z);
+}
+
+float brightness(vec2 uv, vec2 id) {
+    // returns the brightness of a scale, based on its id
+	float t = iTime;
+    float n = hash12(id);
+    float c = mix(0.7, 1., n);				// random variation
     
- 
+    float x = abs(id.x-SIZE*.5);
+    float stripes = sin(x*.65+sin(id.y*.5)+.3)*.5+.5;		// pattern
+    stripes = pow(stripes, 4.);
+    c *= 1.-stripes*.5;
+    
+    float y = floor(uv.y*SIZE);
+    float twinkle = sin(t+n*6.28)*.5 +.5;
+    twinkle = pow(twinkle, 40.);
+    //c += twinkle*.9;                 
+    
+    return c;
+}
+
+float spokes(vec2 uv, float spikeFrequency) {
+	// creates spokes radiating from the top of the scale
+    
+    vec2 p = vec2(0., 1.);
+    vec2 d = p-uv;
+    float l = length(d);
+    d /= l;
+    
+    vec2 up = vec2(1., 0.);
+    
+    float c = dot(up, d);
+    c = abs(sin(c*spikeFrequency));
+    c *= l;
+    
+    return c;
+}
+
+vec4 ScaleInfo(vec2 uv, vec2 p, vec3 shape) {
+    
+    float spikeAmount = shape.x;
+    float spikeFrequency = shape.y;
+    float sag = shape.z;
+    
+    uv -= p;
+    
+    uv = uv*2.-1.;
+  	
+    float d2 = spokes(uv, spikeFrequency);
+    
+    uv.x *= 1.+uv.y*sag;
+   
+	float d1 = dot(uv, uv);					// distance to the center of the scale
+   
+    float threshold = 1.;//sin(iTime)*.5 +.5;
+    
+    float d = d1+d2*spikeAmount;
+    
+    float f = 0.05;//fwidth(d);
+    float c = smoothstep(threshold, threshold-f, d);
+    
+    return vec4(d1, d2, d, c);
+}
+
+vec4 ScaleCol(vec2 uv, vec2 p, vec4 i) {
+    
+    vec3 col1 = vec3(.1, .3, .2);
+    vec3 col2 = vec3(.8, .5, .2);
+    vec3 baseCol = vec3(.1, .3, .2)*3.;
+    uv-=p;
+    
+    float grad = 1.-uv.y;
+    float col = grad+i.x;
+    col = col*.2+.5;
+    
+    vec4 c = vec4(col*baseCol, i.a);
+    
+    c.rgb = mix(c.rgb, col1, i.y*i.x*.5);		// add spokes
+    c.rgb = mix(c.rgb, col2, i.x);				// add edge highlights
+            
+    c = mix(vec4(0.), c, i.a);
+    
+    float fade = 0.3;
+    float shadow = smoothstep(1.+fade, 1., i.z);
+  
+	c.a = mix(shadow*.25, 1., i.a);
+    
+    return c;
+}
 
 
- 
+vec4 Scale(vec2 uv, vec2 p, vec3 shape) {
+    
+    vec4 info = ScaleInfo(uv, p, shape);
+    vec4 c = ScaleCol(uv, p, info);
+    
+    return c;
+}
 
- 
-vec3 rgb2hsv(vec3 c)
+vec4 ScaleTex(vec2 uv, vec2 uv2, vec3 shape) {
+    // id = a vec2 that is unique per scale, can be used to apply effects on a per-scale basis
+    // shape = a vec3 describing the shape of the scale:
+    //			x = the amount of spikyness, can go negative to bulge spikes the opposite way
+    //			y = the number of spikes
+    //          z = the taper of the scale (0=round -1=top wider 1=bottom wider)
+    
+    vec2 id = floor(uv2);
+    uv2 -= id;
+    
+    // need to render a bunch of scales per pixel, so they can overlap
+    vec4 rScale = Scale(uv2, vec2(.5, 0.01), shape);
+   	vec4 lScale = Scale(uv2, vec2(-.5, 0.01), shape);
+    vec4 bScale = Scale(uv2, vec2(0., -0.5), shape);
+    vec4 tScale = Scale(uv2, vec2(0., 0.5), shape);
+    vec4 t2Scale = Scale(uv2, vec2(1., 0.5), shape);
+    
+    // every scale has a slightly different brightness + pattern
+    rScale.rgb *= brightness(uv, id+vec2(1.,0.));
+    lScale.rgb *= brightness(uv, id+vec2(0.,0.));
+    
+    bScale.rgb *= brightness(uv, id+vec2(0.,0.));
+    
+    tScale.rgb *= brightness(uv, id+vec2(0.,1.));
+    t2Scale.rgb *= brightness(uv, id+vec2(2.,1.));
+    
+    // start with base color and alpha blend in all of the scales
+    vec4 c =  vec4(.1, .3, .2,1.);
+    c = mix(c, bScale, bScale.a);
+    c = mix(c, rScale, rScale.a);
+    c = mix(c, lScale, lScale.a);
+    c = mix(c, tScale, tScale.a);
+    c = mix(c, t2Scale, t2Scale.a);
+    
+   // c.rg = uv;
+   // c.b = 0.;
+    return c;
+}
+
+void main()
 {
-    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
-    vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
-    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
-
-    float d = q.x - min(q.w, q.y);
-    float e = 1.0e-10;
-    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
-}
-
-vec3 hsv2rgb(vec3 c)
-{
-    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
-}
-
-float rand(vec2 n) {
-    return fract(sin(cos(dot(n, vec2(12.9898,12.1414)))) * 83758.5453);
-}
-
-float noise(vec2 n) {
-    const vec2 d = vec2(0.0, 1.0);
-    vec2 b = floor(n), f = smoothstep(vec2(0.0), vec2(1.0), fract(n));
-    return mix(mix(rand(b), rand(b + d.yx), f.x), mix(rand(b + d.xy), rand(b + d.yy), f.x), f.y);
-}
-
-float fbm(vec2 n) {
-    float total = 0.0, amplitude = 1.0;
-    for (int i = 0; i <5; i++) {
-        total += noise(n) * amplitude;
-        n += n*1.7;
-        amplitude *= 0.47;
-    }
-    return total;
-}
-
-void main() {
-vec2 offset = vec2(invScale - 1.0) * 0.5;
 float invScale = 1.0 / iScale; // obviously scale must not be zero!
-
-    const vec3 c1 = vec3(0.5, 0.0, 0.1);
-    const vec3 c2 = vec3(0.9, 0.1, 0.0);
-    const vec3 c3 = vec3(0.2, 0.1, 0.7);
-    const vec3 c4 = vec3(1.0, 0.9, 0.1);
-    const vec3 c5 = vec3(0.1);
-    const vec3 c6 = vec3(0.9);
-
-    vec2 speed = vec2(1.2, 0.1);
-    float shift = 1.327+sin(iTime*2.0)/2.4;
-    float alpha = 1.0;
+vec2 offset = vec2(invScale - 1.0) * 0.5;
+    float aspect = iResolution.x/iResolution.y;
     
-    //change the constant term for all kinds of cool distance versions,
-    //make plus/minus to switch between
-    //ground fire and fire rain!
-    float dist = 3.5-sin(iTime*0.4)/1.89;
+	vec2 uv = ( gl_FragCoord.xy / iResolution.xy) * invScale - offset;
+    vec2 m = iMouse.xy/iResolution.xy;
+    m = m*2. - 1.;
+    float t = iTime;
     
-    vec2 p = ( gl_FragCoord.xy * dist / iResolution.xx) * invScale - offset;
-    p.x -= iTime/1.1;
-    float q = fbm(p - iTime * 0.01+1.0*sin(iTime)/10.0);
-    float qb = fbm(p - iTime * 0.002+0.1*cos(iTime)/5.0);
-    float q2 = fbm(p - iTime * 0.44 - 5.0*cos(iTime)/7.0) - 6.0;
-    float q3 = fbm(p - iTime * 0.9 - 10.0*cos(iTime)/30.0)-4.0;
-    float q4 = fbm(p - iTime * 2.0 - 20.0*sin(iTime)/20.0)+2.0;
-    q = (q + qb - .4 * q2 -2.0*q3  + .6*q4)/3.8;
-    vec2 r = vec2(fbm(p + q /2.0 + iTime * speed.x - p.x - p.y), fbm(p + q - iTime * speed.y));
-    vec3 c = mix(c1, c2, fbm(p + r)) + mix(c3, c4, r.x) - mix(c5, c6, r.y);
-    vec3 color = ( vec3(c * cos(shift * gl_FragCoord.y / iResolution.y))) * invScale - offset;
-    color += .05;
-    color.r *= .8;
-    vec3 hsv = rgb2hsv(color);
-    hsv.y *= hsv.z  * 1.1;
-    hsv.z *= hsv.y * 1.13;
-    hsv.y = (2.2-hsv.z*.9)*1.20;
-    color = hsv2rgb(hsv);
-    gl_FragColor = vec4(color.x, color.y, color.z, alpha);
+    
+    
+   	uv.x += sin(t+uv.y)*.1;
+    
+    uv.x*=2.;
+    
+    uv -=.5;
+    
+    
+    vec2 uv2 = uv * SIZE;						// uv2 -2.5 - 2.5
+    uv2.y -= t;
+    uv +=.5;
+       
+    float grad = (uv2.y+12.5)/15.;				// goes from 0-1 
+
+    vec3 center = vec3(.6, 1., -.8);  			// scale shape settings for center -> amount frequency sag
+    vec3 outside = vec3(0.1, 8., -.9);  		// settings for side
+    
+    float sideFade = pow(uv.x-1.,2.);
+    vec3 shape = mix(center, outside, sideFade);// morph between scale shapes
+    
+   // shape = mix(center, outside, 1.);
+    
+  
+    
+    vec4 c = ScaleTex(uv, uv2, shape);			// sample scales
+    c = mix(c, vec4(c.g), sideFade);			// fade color towards the edges
+    
+    t*=.1;										// rainbow....
+    c.r += sin(t)*.4;
+    c.g -= abs(sin(t*1.324))*.435;
+    c.b += sin(t*0.324)*.3;
+    
+    float y = pow(uv.y-.5,2.)*4.;				// vignette
+    c *= 1.-y;
+	
+    gl_FragColor = vec4(c);
 }
+
+
