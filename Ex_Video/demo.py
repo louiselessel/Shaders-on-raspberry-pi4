23cbf8d7974e89b874eecd7a8cb0004c799a2bf8
Louise Lessél <louiselessel@gmail.com>
2025-07-27 18:40:57 +0200

    Update demo.py

--- Ex_Video/demo.py
+++ Ex_Video/demo.py
@@ -1,6 +1,6 @@
 import sys
-# has to point to a downloaded version of pi3d, not the librar
-sys.path.insert(1, '/home/pi/pi3d')
+sys.path.insert(1, '/home/pi/pi3d/src/')
 
+# has to point to a downloaded version of pi3d, not the library
 # this does not work
 #sys.path.insert(1, '/usr/local/lib/python3.7/dist-packages/pi3d') 
